420908bc65b4778becf11ca3062467be2299df38
ArchticStyx <88860163+ArchticStyx@users.noreply.github.com>
2022-02-07 18:48:58 -0600

    Add files via upload

---
+++ Assessment_Challenge.py
@@ -0,0 +1,17 @@
+#My name is Trevor White, this is my assessment challenge
+
+first = "trevor"
+last = "white"
+
+gradeA = "89"
+gradeB = "67"
+gradeC = "83"
+gradeD = "60"
+gradeE = "56"
+gradeF = "59"
+
+full_name = (first + last)
+full_name_cap = (first.title() + " " + last.title())
+f"Your quiz grades are, {gradeA}, {gradeB}, {gradeC}, {gradeD}, {gradeE}, {gradeF}."
+f"{full_name_cap}'s grade point average is " (gradeA + gradeB + gradeC + gradeD + gradeE + gradeF)/6 
+ 
