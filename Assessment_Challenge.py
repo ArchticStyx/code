322546817f1aed548e3554415ea51ec36eee5446
Trevor White <88860163+ArchticStyx@users.noreply.github.com>
2022-02-07 19:50:34 -0600

    Add files via upload
    updated file

--- Assessment_Challenge.py
+++ Assessment_Challenge.py
@@ -3,15 +3,25 @@
 first = "trevor"
 last = "white"
 
-gradeA = "89"
-gradeB = "67"
-gradeC = "83"
-gradeD = "60"
-gradeE = "56"
-gradeF = "59"
+gradeA = 89
+gradeB = 67
+gradeC = 83
+gradeD = 60
+gradeE = 56
+gradeF = 59
+#He didn't do so great in school...
 
 full_name = (first + last)
 full_name_cap = (first.title() + " " + last.title())
-f"Your quiz grades are, {gradeA}, {gradeB}, {gradeC}, {gradeD}, {gradeE}, {gradeF}."
-f"{full_name_cap}'s grade point average is " (gradeA + gradeB + gradeC + gradeD + gradeE + gradeF)/6 
- 
+
+print(f"Your quiz grades are, {gradeA}, {gradeB}, {gradeC}, {gradeD}, {gradeE}, {gradeF}.")
+gpa = (gradeA + gradeB + gradeC + gradeD + gradeE + gradeF) / 6
+print(f"{full_name_cap}'s grade point average is {gpa}")
+
+#Had some trouble getting lines 18, 19 to work. Originally I tried putting the "grades" into an f string, couldn't get the
+#whole string to work. Something like print(f"{full_name}'s grade point average is " (gradeA + gradeB) / 6) which it didn't like
+
+new_gradeA = gradeA + 5
+new_gpa = (new_gradeA + gradeB + gradeC + gradeD + gradeE + gradeF) / 6
+print(f"{full_name_cap}'s new grade point average is {new_gpa}")
+#Couldn't manage to get the C :(
